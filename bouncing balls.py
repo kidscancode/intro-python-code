b7f9d24d711892fd8033a2b8d458bb7e04ef2405
Chris Bradfield <chris@kidscancode.org>
2016-06-22 12:17:24 -0700

    edits

--- bouncing balls.py
+++ bouncing balls.py
@@ -1,50 +1,39 @@
-# Animated bouncing balls
-# KidsCanCode - Intro to Programming
 from tkinter import *
-import random
 import time
+import random
 
-# Define ball properties and functions
-class Ball:
-    def __init__(self, canvas, color, size):
-        self.canvas = canvas
-        self.id = canvas.create_oval(10,10,size,size, fill=color)
-        self.canvas.move(self.id, 245, 100)
-        self.yspeed = -1
-        self.xspeed = random.randrange(-3, 3)
+WIDTH = 800
+HEIGHT = 500
 
-    def draw(self):
-        self.canvas.move(self.id, self.xspeed, self.yspeed)
-        pos = self.canvas.coords(self.id)
-        if pos[1] <= 0:
-            self.yspeed = random.randrange(1,10)
-        if pos[3] >= 400:
-            self.yspeed = random.randrange(-10,-1)
-        if pos[0] <= 0:
-            self.xspeed = random.randrange(1,10)
-        if pos[2] >= 500:
-            self.xspeed = random.randrange(-10,-1)
-
-# Create window and canvas to draw on
 tk = Tk()
-tk.title("Game")
-canvas = Canvas(tk, width=500, height=400, bd=0)
+canvas = Canvas(tk, width=WIDTH, height=HEIGHT, bg="black")
+tk.title("Drawing")
 canvas.pack()
-tk.update()
 
-# Create a list of 20 balls, each with a random size and color
+colors = ['red', 'green', 'blue', 'orange', 'yellow', 'cyan', 'magenta',
+          'dodgerblue', 'turquoise', 'grey', 'gold', 'pink']
+
+class Ball:
+    def __init__(self):
+        self.size = random.randrange(200, 400)
+        color = random.choice(colors)
+        self.shape = canvas.create_rectangle(0, 0, self.size, self.size, fill=color)
+        self.speedx = random.randrange(1, 10)
+        self.speedy = random.randrange(1, 10)
+
+    def update(self):
+        canvas.move(self.shape, self.speedx, self.speedy)
+        pos = canvas.coords(self.shape)
+        if pos[2] >= WIDTH or pos[0] <= 0:
+            self.speedx *= -1
+        if pos[3] >= HEIGHT or pos[1] <= 0:
+            self.speedy *= -1
+
 ball_list = []
-color_list = ['blue', 'red', 'green', 'papaya whip', 'brown', 'lavender']
-for i in range(20):
-    color = random.choice(color_list)
-    size = random.randrange(10, 100)
-    ball_list.append(Ball(canvas, color, size))
-
-# Animation loop
+for i in range(100):
+    ball_list.append(Ball())
 while True:
-    # animate each ball in the list of balls
     for ball in ball_list:
-        ball.draw()
-    tk.update_idletasks()
+        ball.update()
     tk.update()
     time.sleep(0.01)
